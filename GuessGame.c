94a76f7ffcd00727d9150563194a293f4f322605
mayela101 <113002207+mayela101@users.noreply.github.com>
2022-10-23 21:01:34 -0700

    Add files via upload

---
+++ GuessGame.c
@@ -0,0 +1,29 @@
+
+// Number Guessing Game
+
+/* do-while loop to allow the user
+to play as long as they want until 
+they wish to exit */
+
+// display menu
+
+//use switches and condition will be
+// based off user input
+
+//use rand() to output random number
+
+
+//quit functionality 
+
+//if not quit start game 
+
+//implement "hot" and "cold " comments
+//to direct user to the correct number
+
+
+//add break to exit the game
+
+//new case to change the max number
+
+//value must be between 0 and 10 
+
